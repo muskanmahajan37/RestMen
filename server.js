c4619363b6a65b0b5665c0565f99c7b07b471330
Sunny Dhoke <sunnydhoke22@gmail.com>
2020-11-05 17:32:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 //server code
+
+// env
+require("dotenv").config({ path: ".env" });
+
+//libs
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+//connect to mongodb
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.on("Error", (error) => console.error(error));
+db.on("Opened Successfully", () => console.log("Connected to database"));
+
+app.listen(300, () => console.log("Server Running!"));
